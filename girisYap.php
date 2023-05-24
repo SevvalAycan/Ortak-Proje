ca4896e7d412559942173e014b17390f2707f190
SevvalAycan <116665967+SevvalAycan@users.noreply.github.com>
2023-05-24 12:34:38 +0300

    Update girisYap.php

--- girisYap.php
+++ girisYap.php
@@ -3,60 +3,52 @@ include("baglanti.php");
 
 $name_err = "";
 $sifre_err = "";
+
 if (isset($_POST["giris"])) {
-    //Kullanıcı adı doğrulama
+    // Kullanıcı adı doğrulama
     if (empty($_POST["kullanici"])) {
-        $name_err = "Kullanıcı adı boş geçilmez";
+        $name_err = "Kullanıcı adı boş geçilemez";
     } else if (strlen($_POST["kullanici"]) < 6) {
         $name_err = "Kullanıcı adı altı harften boş olamaz";
     } else if (!preg_match('/^[a-z\d_]{5,20}$/i', $_POST["kullanici"])) {
-        echo "Sadece harfler ve sayılar kullanınız.";
+        $name_err = "Sadece harfler ve sayılar kullanılabilir";
     } else {
         $name = $_POST["kullanici"];
     }
-    //Sifre Doğrulama
+
+    // Şifre doğrulama
     if (empty($_POST["sifre"])) {
-        $sifre_err = "Şifre boş geçilmez";
+        $sifre_err = "Şifre boş geçilemez";
     } else {
         $password = $_POST["sifre"];
     }
 
-
-
-
-
-
-    //Giriş Eylemi
-    $name = $_POST["kullanici"];
-    $password = $_POST["sifre"];
-
-    if (isset($name) && isset($password)) {
-
-        $secim = "SELECT * FROM kullanicilar WHERE kullanici ='$name ' ";
+    // Giriş Eylemi
+    if (isset($name) && isset($password)) { // Değişken adını $sifre olarak değiştirildi.
+        $secim = "SELECT * FROM kullanicilar WHERE kullanici ='$name'";
         $calistir = mysqli_query($baglanti, $secim);
         $kayitsayisi = mysqli_num_rows($calistir);
+
         if ($kayitsayisi > 0) {
             $ilgilikayit = mysqli_fetch_assoc($calistir);
-            $sifreler = $ilgilikayit["sifre"];
+            $hashlisifre = $ilgilikayit["sifre"]; // Değişken adını $hashlisifre olarak değiştirildi.
 
-            if (password_verify($parola, $sifreler)) {
+            if (password_verify($password, $hashlisifre)) {
                 session_start();
                 $_SESSION["kullanici"] = $ilgilikayit["kullanici"];
-                $_SESSION["email"] = $ilgilikayit["kullanici"];
-                header("location:profile.php");
+                $_SESSION["email"] = $ilgilikayit["email"];
+                header("location: profile.php");
+                exit();
             } else {
-                echo '<script>alert("Şifreyi yada Kullanıcı Adını yanlış girdiniz")</script>';
+                echo '<script>alert("Şifreyi veya Kullanıcı Adını yanlış girdiniz")</script>';
             }
         } else {
-            echo '<script>alert("Şifreyi yanlış girdiniz")</script>';
+            echo '<script>alert("Kullanıcı adı bulunamadı")</script>';
         }
     }
     mysqli_close($baglanti);
 }
-
 ?>
-
-
 <!DOCTYPE html>
 <html>
 
@@ -65,11 +57,9 @@ if (isset($_POST["giris"])) {
     <title>Giriş Yap</title>
     <link rel="icon" type="image/png" href="neonlogo.png">
     <link rel="stylesheet" href="style.css">
-
 </head>
 
 <body>
-
     <nav class="nav-container">
         <img src="neonlogo.png" width="50" height="50" alt="Web site logo" id="logo" style="cursor: pointer;"
             class="logo" />
@@ -82,37 +72,33 @@ if (isset($_POST["giris"])) {
 
         <ul id="nav-links">
             <li><a href="neonblog.html" class="links">Anasayfa</a></li>
-            <li><a href="#" class="links">Forum</a></li>
+            <li><a href="profile.php" class="links">Profil</a></li>
             <li><a href="https://www.bandirma.edu.tr/tr/www/Iletisim" class="links">İletişim</a></li>
             <li><a href="giris-yap.php" class="links">Giriş Yap</a></li>
             <li><a href="kaydol.php" class="links">Kayıt Ol</a></li>
         </ul>
-
     </nav>
 
     <div class="container">
         <form action="giris-yap.php" method="POST">
             <h2>Giriş Yap</h2>
-            <label for=kullanici>Kullanıcı Adı</label>
-            <input type="text" name="kullanici" class="form-control is invalid" placeholder="Kullanıcı Adı" required>
+            <label for="kullanici">Kullanıcı Adı</label>
+            <input type="text" name="kullanici"
+                class="form-control <?php echo (!empty($name_err)) ? 'is-invalid' : ''; ?>" placeholder="Kullanıcı Adı"
+                required>
             <div class="invalid-feedback">
-                <?php
-                echo $name_err;
-                ?>
+                <?php echo $name_err; ?>
             </div>
             <label for="sifre">Şifre</label>
-            <input type="password" name="sifre" class="form-control is invalid" placeholder="Şifre" required>
+            <input type="password" name="sifre"
+                class="form-control <?php echo (!empty($sifre_err)) ? 'is-invalid' : ''; ?>" placeholder="Şifre"
+                required>
             <div class="invalid-feedback">
-                <?php
-                echo $sifre_err;
-                ?>
+                <?php echo $sifre_err; ?>
             </div>
-
-
             <button type="submit" name="giris" id="button">Giriş Yap</button>
         </form>
-        <p>Hesabın yok mu? <a href="kaydol.html">Kaydol</a></p>
-
+        <p>Hesabın yok mu? <a href="kaydol.php">Kaydol</a></p>
     </div>
 </body>
 
