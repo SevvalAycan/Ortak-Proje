cba68d6b5f25dd97e44b1d8a6795737a806f1eec
SevvalAycan <116665967+SevvalAycan@users.noreply.github.com>
2023-05-24 12:34:59 +0300

    Update baglanti.php

--- baglanti.php
+++ baglanti.php
@@ -7,4 +7,8 @@ $vt = "bandirmamühendisliği";
 
 $baglanti = mysqli_connect($host, $kullanici, $parola, $vt);
 mysqli_set_charset($baglanti, "UTF8");
+if (!$baglanti) {
+    die("Veritabanı bağlantısı başarısız oldu: " . mysqli_connect_error());
+}
+
 ?>
